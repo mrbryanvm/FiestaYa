d3a3cc8cb24d57d88e54593a29533daf6053d47c
HP <hp@hp-HP-Laptop-15-db0xxxexample.com>
2025-05-20 17:16:33 -0400

    Configuración inicial del proyecto Konekta y ajustes en AndroidManifest

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,36 @@
+// android/build.gradle.kts
+
+import org.gradle.api.Project
+import org.gradle.api.tasks.Delete
+import org.gradle.api.file.Directory
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("com.google.gms:google-services:4.4.0")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
